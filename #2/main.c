7e2b130b56ff894e16d26715e77fe3df69a4d027
Sakura <c1531055@eng.kitakyu-u.ac.jp>
2023-11-24 13:59:20 +0900

    [add]update  #2(第二回演習)

---
+++ #2/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    //helloworldを出力する
+    printf("helloworld!!!!!!!!!!!!!!!!");
+    printf("\n");
+    printf("こんにちは私はコンピュータです");
+    printf("\n");
+    printf("I'm\tfrom\tFuluoka\tprefecter\n");
+    printf("I'm\tfrom\tTokyo \tprefecter\n");
+    return 0;
+}
